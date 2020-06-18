1e7510709e383a45e45afe8f49c3fd0e0bb90617
Matt Clyde <mclyde@nextcenturymeters.com>
2020-06-18 10:11:39 -0600

    refactor: added some basic typing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import * as child from 'child_process';
 import * as fs from 'fs';
-import * as path from 'path';
 import * as _ from 'lodash';
 import * as program from 'commander';
-import { Types } from './types';
+import { CommitTypes, ICommitType } from './types';
 
 program.option('-f, --from <tag>', 'From tag');
 program.parse(process.argv);
 
-const groups = {};
+const packagePath = './package.json';
+
+interface ICommit {
+  author: string;
+  sha: string;
+  type: string;
+  message: string;
+  context: string;
+}
+
+const groups: { [type: string]: { [context: string]: ICommit[] } } = {};
 let version = 'unknown';
-const packagePath = path.join(__dirname, '/package.json');
 if (fs.existsSync(packagePath)) {
   const packageObj = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
   version = packageObj.version;
@@ -23,15 +31,9 @@ function run() {
   }
 
   const output = child.execSync(`git log ${program.from}..HEAD --format="%H [%cn] %s"`).toString('utf-8');
-  const commits = output.split('\n')
-    .map(raw => {
-      const commit = {
-        author: '',
-        sha: '',
-        type: '',
-        context: '',
-        message: '',
-      };
+  const commits: ICommit[] = output.split('\n')
+    .map((raw) => {
+      const commit: ICommit = { author: '', sha: '', type: '', message: '', context: '' };
       const parts = /^(\S+)\s+\[(.+)\]\s+(\w+)(\(.+\))?:\s+(.+)$/gi.exec(raw);
       if (parts && parts.length === 6) {
         commit.sha = parts[1];
@@ -42,9 +44,9 @@ function run() {
       }
       return commit;
     })
-    .filter(commit => !!commit.sha);
+    .filter((commit) => !!commit.sha);
 
-  _.forEach(commits, (commit) => {
+  _.forEach(commits, (commit: ICommit) => {
     groups[commit.type] = groups[commit.type] || {};
     groups[commit.type][commit.context] = groups[commit.type][commit.context] || [];
     groups[commit.type][commit.context].push(commit);
@@ -54,9 +56,9 @@ function run() {
     new Date().toISOString().split('T')[0]
     })\n\n`;
 
-  _.forEach(Types, (type) => {
+  _.forEach(CommitTypes, (type: ICommitType) => {
     if (groups[type.id]) {
-      changelog += `## ${type.name}\n\n`;
+      changelog += `\n## ${type.name}\n\n`;
       const contexts = Object.keys(groups[type.id]).sort().filter((context) => context !== 'null');
       _.forEach(groups[type.id]['null'], (commit) => changelog += `* ${formatCommit(commit)}\n`);
       _.forEach(contexts, (context) => {
@@ -68,14 +70,14 @@ function run() {
     }
   });
 
-  const changeLogsPath = path.join(__dirname, '/changelogs')
+  const changeLogsPath = './changelogs';
   if (!fs.existsSync(changeLogsPath)) {
     fs.mkdirSync(changeLogsPath);
   }
   fs.writeFileSync(`${changeLogsPath}/${version}.md`, changelog);
 }
 
-function formatCommit(commit) {
+function formatCommit(commit: ICommit) {
   return `${commit.message} [[${commit.author}](https://github.com/NextCenturyMeters/ncss-cloud/commit/${commit.sha})]`;
 }
 
